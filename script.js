e911999e9fa30946c52216cee8b8295b6a9dc6ab
ruben <174733720+ruben-23@users.noreply.github.com>
2025-08-08 23:58:32 +0300

    Change the gameboard variable to a matrix; Update the showGameboard and updateGameboard functions to reflect this change

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const game = (function Gameboard() {
     let gameboard = [];
+    const gameboardSize = 3;
 
     const getGameboard = function () {
         return gameboard;
@@ -10,23 +11,40 @@ const game = (function Gameboard() {
         gameboard = [];
     }
 
+    const initGameBoard = function () {
+        for (let i = 0; i < gameboardSize; ++i) {
+            gameboard[i] = [];
+            for (let j = 0; j < gameboardSize; ++j) {
+                gameboard[i][j] = Cell();
+            }
+        }
+    }
+
     const addCellToGameboard = function (cell) {
         gameboard.push(cell)
     }
 
     const showGameboard = function () {
-        for (const cell of gameboard) {
-            console.log('___');
-            console.log('|' + cell.getMark() + '|');
-            console.log('___');
+        console.log('------------- START -------------------');
+        for (const row of gameboard) {
+            console.log('---------------- ' + gameboard.indexOf(row) + ' -----------------');
+            for (const cell of row) {
+                console.log(row.indexOf(cell) + ' |' + cell.getMark() + '|');
+            }
+
+        }
+        console.log('------------- END -------------------');
+    }
+
+    const updateGameboard = function (row, column, userMark) {
+        if (!gameboard[row][column].getMark()) {
+            gameboard[row][column].putMark(userMark);
+        } else {
+            console.log('Cell taken. Please chose another cell');
         }
     }
 
-    const updateGameboard = function(cellIndex, userMark){
-        gameboard[cellIndex].putMark(userMark);
-    }
-
-    return { getGameboard, clearBoard, addCellToGameboard, showGameboard, updateGameboard };
+    return { getGameboard, clearBoard, addCellToGameboard, showGameboard, updateGameboard, initGameBoard };
 
 })();
 
@@ -46,26 +64,15 @@ function Cell() {
 }
 
 const gameController = (function GameController() {
-    const ROWS = 3;
-    const COLS = 3;
 
-    for (let i = 0; i < ROWS; ++i) {
-        for (let j = 0; j < COLS; ++j) {
-            game.addCellToGameboard(Cell());
-        }
-    }
-
-    // for (const cell of game.getGameboard()) {
-    //     console.log('___');
-    //     console.log('|' + cell.getMark() + '|');
-    //     console.log('___');
-    // }
-
-    game.updateGameboard(5, 'O');
-    game.updateGameboard(4, 'X');
+    game.initGameBoard();
 
     game.showGameboard();
 
+    game.updateGameboard(1, 1, 'X');
+    game.showGameboard();
+    game.updateGameboard(1, 1, 'O');
+    game.showGameboard();
 
 })();
 
