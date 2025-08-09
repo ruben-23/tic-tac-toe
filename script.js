79249bbbc8f92353e1ba917245a1fd116c988da0
ruben <174733720+ruben-23@users.noreply.github.com>
2025-08-09 13:34:34 +0300

    Add functionality to check for the winner on rows, columns and diagonals

--- script.js
+++ script.js
@@ -36,6 +36,72 @@ const game = (function Gameboard() {
         console.log('------------- END -------------------');
     }
 
+
+    const checkRow = function (rowIndex) {
+        console.log('checking rows');
+        for (let i = 0; i < gameboardSize - 1; ++i) {
+            const currentCell = gameboard[rowIndex][i];
+            const nextCell = gameboard[rowIndex][i + 1];
+
+            if (currentCell.getMark() !== nextCell.getMark()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const checkColumn = function (colIndex) {
+        console.log('checking columns');
+        for (let i = 0; i < gameboardSize - 1; ++i) {
+            const currentCell = gameboard[i][colIndex];
+            const nextCell = gameboard[i + 1][colIndex];
+            if (currentCell.getMark() !== nextCell.getMark()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const checkPrimaryDiagonal = function () {
+        console.log('checking primary diagonal');
+        for (let i = 0; i < gameboardSize - 1; ++i) {
+            const currentCell = gameboard[i][i];
+            const nextCell = gameboard[i + 1][i + 1];
+            if (currentCell.getMark() !== nextCell.getMark()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const checkSecondaryDiagonal = function () {
+        console.log('checking secondary diagonal');
+        for (let i = 0; i < gameboardSize - 1; ++i) {
+            const currentCell = gameboard[i][gameboardSize - 1 - i];
+            const nextCell = gameboard[i + 1][gameboardSize - 1 - (i + 1)];
+            if (currentCell.getMark() !== nextCell.getMark()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    // if winner found -> return winner mark 
+    // else -> return false
+    const checkWin = function () {
+
+        for (let i = 0; i < gameboardSize; ++i) {
+            // check win on rows and columns -> return the winner(X or O)
+            if (checkRow(i)) return gameboard[i][0].getMark();
+            if (checkColumn(i)) return gameboard[0][i].getMark()
+        }
+
+        if (checkPrimaryDiagonal()) return gameboard[0][0].getMark();
+        if (checkSecondaryDiagonal()) return gameboard[0][gameboardSize - 1].getMark();
+        return false;
+    }
+
     const updateGameboard = function (row, column, userMark) {
         if (!gameboard[row][column].getMark()) {
             gameboard[row][column].putMark(userMark);
@@ -44,7 +110,7 @@ const game = (function Gameboard() {
         }
     }
 
-    return { getGameboard, clearBoard, addCellToGameboard, showGameboard, updateGameboard, initGameBoard };
+    return { getGameboard, clearBoard, addCellToGameboard, showGameboard, updateGameboard, initGameBoard, checkWin };
 
 })();
 
@@ -67,12 +133,16 @@ const gameController = (function GameController() {
 
     game.initGameBoard();
 
+    // testing
+    game.updateGameboard(0, 0, 'X');
+    game.updateGameboard(1, 0, 'X');
+    game.updateGameboard(2, 0, 'X');
+    
     game.showGameboard();
 
-    game.updateGameboard(1, 1, 'X');
-    game.showGameboard();
-    game.updateGameboard(1, 1, 'O');
-    game.showGameboard();
+    const winner = game.checkWin();
+    if (winner) console.log('Winner: ', winner);
+
 
 })();
 
